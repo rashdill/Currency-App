74271e45245ff61199251a21504a24acae05e31b
Rashmi Herath <diliherath1018@gmail.com>
2024-04-29 16:48:59 +0530

    h1 and p

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Convert Your Currencies Today</h1>
+      <p>
+        Welcome to "Convert Your Currencies Today!" This application allows you to easily convert currencies based on the latest exchange rates.
+        Wheather you are planning a trip,managing your finances or simply curious about the value of your money in differentt currencies, this tool is here to help. 
+      </p>
+    </div>
+  )
+}
