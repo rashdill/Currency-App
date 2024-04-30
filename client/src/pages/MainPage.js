c281ef44c06a51d4c82ce7d65dce401dccfe9531
Rashmi Herath <diliherath1018@gmail.com>
2024-04-30 13:35:50 +0530

    assign value for source currency fileld

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -35,15 +35,16 @@ export default function MainPage() {
             </div>
             {/*This is Source Currency field */}
             <div className="mb-4">
-              <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+              <label htmlFor="sourceCurrency" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                 Source Currency
               </label>
               <select
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
                 placeholder="name@flowbite.com"
                 required
-                name=""
-                id=""
+                name={sourceCurrency}
+                id={sourceCurrency}
+                value={sourceCurrency}
               >
                 <option value="">
                   Select the Source Currency</option>
