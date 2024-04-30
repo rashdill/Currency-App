f9e6bd066c654ee70c860ef75e898700443f48fe
Rashmi Herath <diliherath1018@gmail.com>
2024-04-30 13:04:35 +0530

    target currency field

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -40,7 +40,22 @@ export default function MainPage() {
                   Select the Source Currency</option>
               </select>
             </div>
-            
+            {/*This is Target Currency field */}
+            <div className="mb-4">
+              <label htmlFor="" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Target Currency
+              </label>
+              <select
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+                placeholder="name@flowbite.com"
+                required
+                name=""
+                id=""
+              >
+                <option value="">
+                  Select the Target Currency</option>
+              </select>
+            </div>
           </form>
         </section>
       </div>
