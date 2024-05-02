2c716dfeb68e07bbdd25ae41d1e9cfe97ac8b6a9
Rashmi Herath <diliherath1018@gmail.com>
2024-05-02 12:41:19 +0530

    INDEX.JS setup the server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-console.log("hello Rashmi");
+const express= require("express");
+const cors=require("cors");
+const axios=require("axios");
+
+const app=express();
+//middle wares
+app.use(express.json());
+app.use(cors());
+
+//listen to a port
+app.listen(5000,()=>){
+    console.log("SERVER STARTED");
+
+}
