9e364ac0a6f344299b5b826012bbe6b80385cf7f
Rashmi Herath <diliherath1018@gmail.com>
2024-05-02 13:16:24 +0530

    all currency func

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,18 @@ const app=express();
 //middle wares
 app.use(express.json());
 app.use(cors());
+//all currencies
+app.get("/getAllCurrencies" , async(req,res)=>{
+    const nameURL = "https://docs.openexchangerates.org/reference/currencies-json?app_id=27f4b248342746caa1b35bc8dd82d36a";
+     const nameResponce = await axios.get(nameURL)
+    try{
+
+    }catch(err){
+        console.error(err);
+    }
+})
+
+
 
 //listen to a port
 app.listen(5000, () => {
